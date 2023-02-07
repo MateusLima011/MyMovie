7b265dc089eb2af2dcd4b3e9e4f32a95f85104ee
mateuslima <mateuslima.ml76@gmail.com>
2023-02-07 14:30:27 -0300

    Created Fragments Of Layouts Discover and Search,Created Navigation and Menu

--- app/src/main/java/com/example/mymovie/MainActivity.kt
+++ app/src/main/java/com/example/mymovie/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.mymovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mymovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupViewBinding()
+        setupNavController()
+        setupBottomNavigation()
     }
+
+    private fun setupViewBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_fragment_container
+        ) as NavHostFragment
+        navController = navHostFragment.findNavController()
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+
 }
